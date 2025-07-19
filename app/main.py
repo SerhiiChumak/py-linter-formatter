16ba56f978cb9c3934553603c9bd491d548545ea
Serhii Chumak <s4umak@gmail.com>
2025-07-19 17:22:44 +0300

    Implement the Solution_linter_formater

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ def format_linter_error(error: dict) -> dict:
         "line": error["line_number"],
         "column": error["column_number"],
         "message": error["text"],
-        "name": error["code"],
+        "rule": error["code"],
         "source": "flake8"
     }
 
@@ -17,4 +17,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path, errors) for file_path, errors in linter_report.items()]
+    return [format_single_linter_file(file_path, errors)
+            for file_path, errors in linter_report.items()]
